a77a488659fdcad51b3f8b082ed428deff7c1949
jxxngin <wwd0203@daum.net>
2025-02-12 14:20:02 +0900

    [feat] modify authorization

--- src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -67,7 +67,9 @@ public class ApiV1PostController {
 
     record ModifyReqBody(
             @NotBlank @Length(min = 3) String title,
-            @NotBlank @Length(min = 3) String content
+            @NotBlank @Length(min = 3) String content,
+            @NotNull Long authorId,
+            @NotBlank @Length(min = 3) String password
     ) {
     }
 
@@ -76,7 +78,24 @@ public class ApiV1PostController {
             @PathVariable long id,
             @RequestBody @Valid ModifyReqBody body
     ) {
+        Member actor = memberService.findById(body.authorId()).get();
+
+        if (!actor.getPassword().equals(body.password())) {
+            throw new ServiceException(
+                    "401-1",
+                    "비밀번호가 일치하지 않습니다."
+            );
+        }
+
         Post post = postService.getItem(id).get();
+
+        if (post.getAuthor().getId() != body.authorId()) {
+            throw new ServiceException(
+                    "403-1",
+                    "자신이 작성한 글만 수정 가능합니다."
+            );
+        }
+
         postService.modify(post, body.title(), body.content());
 
         return new RsData<>(
