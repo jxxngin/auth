356b5a82d2cfc22614015091aec58072bab04e5a
jxxngin <wwd0203@daum.net>
2025-02-12 14:16:26 +0900

    [feat] password authentication

--- src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -6,6 +6,7 @@ import com.example.rest.domain.post.post.dto.PostDto;
 import com.example.rest.domain.post.post.entity.Post;
 import com.example.rest.domain.post.post.service.PostService;
 import com.example.rest.global.dto.RsData;
+import com.example.rest.global.expection.ServiceException;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -88,13 +89,22 @@ public class ApiV1PostController {
     record WriteReqBody(
             @NotBlank @Length(min = 3) String title,
             @NotBlank @Length(min = 3) String content,
-            @NotNull Long authorId
+            @NotNull Long authorId,
+            @NotBlank @Length(min = 3) String password
     ) {
     }
 
     @PostMapping
     public RsData<PostDto> write(@RequestBody @Valid WriteReqBody body) {
         Member actor = memberService.findById(body.authorId()).get();
+
+        if (!actor.getPassword().equals(body.password())) {
+            throw new ServiceException(
+                    "401-1",
+                    "비밀번호가 일치하지 않습니다."
+            );
+        }
+
         Post post = postService.write(actor, body.title(), body.content());
 
         return new RsData<>(
