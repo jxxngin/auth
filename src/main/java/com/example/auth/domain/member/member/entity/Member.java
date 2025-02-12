2f8b45f9eb85b53c574fbc11583b3668b55f96d6
jxxngin <wwd0203@daum.net>
2025-02-12 16:46:00 +0900

    [feat] admin modify

--- src/main/java/com/example/auth/domain/member/member/entity/Member.java
+++ src/main/java/com/example/auth/domain/member/member/entity/Member.java
@@ -28,4 +28,8 @@ public class Member extends BaseTime {
     @Column(length = 100)
     private String nickname;
 
+    public boolean isAdmin() {
+        return username.equals("admin");
+    }
+
 }
