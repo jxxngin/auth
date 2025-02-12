c6d8271a00873f1cb2aaa1d2d81cf0121cb33058
jxxngin <wwd0203@daum.net>
2025-02-12 14:08:45 +0900

    init

---
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -0,0 +1,105 @@
+package com.example.rest.domain.post.post.controller;
+
+import com.example.rest.domain.member.member.entity.Member;
+import com.example.rest.domain.member.member.service.MemberService;
+import com.example.rest.domain.post.post.dto.PostDto;
+import com.example.rest.domain.post.post.entity.Post;
+import com.example.rest.domain.post.post.service.PostService;
+import com.example.rest.global.dto.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/api/v1/posts")
+@RequiredArgsConstructor
+public class ApiV1PostController {
+
+    private final PostService postService;
+    private final MemberService memberService;
+
+    @GetMapping
+    public RsData<List<PostDto>> getItems() {
+        List<Post> posts = postService.getItems();
+        List<PostDto> postDtos = posts.stream()
+                .map(PostDto::new)
+                .toList();
+
+        return new RsData<>(
+                "200-1",
+                "글 목록 조회가 완료되었습니다.",
+                postDtos
+        );
+    }
+
+    @GetMapping("{id}")
+    public RsData<PostDto> getItem(@PathVariable long id) {
+        Post post = postService.getItem(id).get();
+
+        return new RsData<>(
+                "200-1",
+                "글 조회가 완료되었습니다.",
+                new PostDto(post)
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public RsData<Void> delete(@PathVariable long id) {
+        Post post = postService.getItem(id).get();
+        postService.delete(post);
+
+        return new RsData<>(
+                "204-1",
+                "%d번 글 삭제가 완료되었습니다.".formatted(id)
+        );
+    }
+
+    record ModifyReqBody(
+            @NotBlank @Length(min = 3) String title,
+            @NotBlank @Length(min = 3) String content
+    ) {
+    }
+
+    @PutMapping("{id}")
+    public RsData<Void> modify(
+            @PathVariable long id,
+            @RequestBody @Valid ModifyReqBody body
+    ) {
+        Post post = postService.getItem(id).get();
+        postService.modify(post, body.title(), body.content());
+
+        return new RsData<>(
+                "200-1",
+                "%d번 글 수정이 완료되었습니다.".formatted(id),
+                null
+        );
+    }
+
+    record WriteReqBody(
+            @NotBlank @Length(min = 3) String title,
+            @NotBlank @Length(min = 3) String content
+    ) {
+    }
+
+    @PostMapping
+    public RsData<PostDto> write(@RequestBody @Valid WriteReqBody body) {
+        Member actor = memberService.findByUsername("user3").get();
+        Post post = postService.write(actor, body.title(), body.content());
+
+        return new RsData<>(
+            "200-1",
+            "글 작성이 완료되었습니다.",
+            new PostDto(post)
+        );
+    }
+
+}
